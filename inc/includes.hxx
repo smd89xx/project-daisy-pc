2778fe38eb1e1b337b54f424e504a4e7b7761466
TheWindowsPro98 <git-twp98@hotmail.com>
2023-09-10 19:04:12 -0500

    Fades
    cool transitions that took way too long to implement

--- inc/includes.hxx
+++ inc/includes.hxx
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <iostream>
+#include <functional>
 #include "types.hxx"
 #include "resources.hxx"
 #include "version.hxx"
 #include "structs.hxx"
+#include "prefs.hxx"
 #include "SFML/Window.hpp"
 #include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
--- inc/resources.hxx
+++ inc/resources.hxx
@@ -8,5 +8,7 @@ extern const std::string titleImg;
 extern const std::string cursorImg;
 extern const std::string blazeTTF;
 extern const std::string lvlClearTrack;
-extern const std::string menuSFX;
+extern const std::string hoverSFX;
 extern const std::string titleTrack;
+extern const std::string lfTrack;
+extern const std::string confSFX;
